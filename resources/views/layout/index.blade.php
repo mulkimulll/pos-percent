4534a7951878dc7bbfc3d82d934d921434eaee36
mulkimulll <mulkizakizainray@gmail.com>
2021-11-07 22:27:16 +0700

    perubahan logo

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PERCENT COFFEE</title>
+    <title>PakKasir</title>
 
+    <link rel="icon" type="img/png" href="{{asset('images/pakkasir-logo.png')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,8 +1,8 @@
 <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
         <a href="{{url('/')}}" class="navbar-brand">
-            <img src="{{asset('img/logo.jpg')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Percent Coffee</span>
+            <img src="{{asset('images/pakkasir-logo.png')}}" alt="logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">PakKasir</span>
         </a>
 
         <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse"
